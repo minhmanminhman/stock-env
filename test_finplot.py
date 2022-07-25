dbc93df75c9bdade18d0de08c02c3583cf990b04
manbnm <manbnm@ssi.com.vn>
2022-07-25 20:38:27 +0700

    update

--- test_finplot.py
+++ test_finplot.py
@@ -1,43 +1,44 @@
 import finplot as fplt
 import numpy as np
+import mt4_hst
 import pandas as pd
-import requests
+import pandas_ta as ta
 
-# pull some data
-symbol = 'USDT-BTC'
-url = 'https://bittrex.com/Api/v2.0/pub/market/GetTicks?marketName=%s&tickInterval=fiveMin' % symbol
-data = requests.get(url).json()
+####
+import finplot as fplt
+import yfinance
 
-# format it in pandas
-df = pd.DataFrame(data['result'])
-df = df.rename(columns={'T':'time', 'O':'open', 'C':'close', 'H':'high', 'L':'low', 'V':'volume'})
-df = df.astype({'time':'datetime64[ns]'})
+df = yfinance.download('AAPL')
+df[['Open', 'Close', 'High', 'Low']]
+print(df.head())
+###
 
-# create two axes
-ax,ax2 = fplt.create_plot(symbol, rows=2)
+# # pull some data
+# symbol = 'FPT'
 
-# plot candle sticks
-candles = df[['time','open','close','high','low']]
-fplt.candlestick_ochl(candles, ax=ax)
+# # format it in pandas
+# df = mt4_hst.read_hst("stock_env/datasets/FPT1440.hst")
+# df.ta.rsi(append=True)
+# df.dropna(inplace=True)
 
-# overlay volume on the top plot
-volumes = df[['time','open','close','volume']]
-fplt.volume_ocv(volumes, ax=ax.overlay())
+# # create two axes
+# ax, ax2 = fplt.create_plot(symbol, rows=2)
 
-# put an MA on the close price
-fplt.plot(df['time'], df['close'].rolling(25).mean(), ax=ax, legend='ma-25')
+# # plot candle sticks
+# # candles = df[['time','open','close','high','low']]
+# # fplt.candlestick_ochl(candles, ax=ax)
+# df.plot(kind='candle', ax=ax)
 
-# place some dumb markers on low wicks
-lo_wicks = df[['open','close']].T.min() - df['low']
-df.loc[(lo_wicks>lo_wicks.quantile(0.99)), 'marker'] = df['low']
-fplt.plot(df['time'], df['marker'], ax=ax, color='#4a5', style='^', legend='dumb mark')
+# # overlay volume on the top plot
+# # volumes = df[['time','open','close','volume']]
+# # fplt.volume_ocv(volumes, ax=ax.overlay())
 
-# draw some random crap on our second plot
-fplt.plot(df['time'], np.random.normal(size=len(df)), ax=ax2, color='#927', legend='stuff')
-fplt.set_y_range(-1.4, +3.7, ax=ax2) # hard-code y-axis range limitation
+# # draw some random crap on our second plot
+# # fplt.plot(df['time'], df['RSI_14'], ax=ax2, legend='RSI_14')
+# # fplt.set_y_range(-1.4, +3.7, ax=ax2) # hard-code y-axis range limitation
 
-# restore view (X-position and zoom) if we ever run this example again
-fplt.autoviewrestore()
+# # restore view (X-position and zoom) if we ever run this example again
+# fplt.autoviewrestore()
 
-# we're done
-fplt.show()
+# # we're done
+# fplt.show()
