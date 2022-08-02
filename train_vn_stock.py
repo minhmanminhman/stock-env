3f702bc871e556c0e80a2280ac8aca3512fa77cc
minhmanminhman <buingocminhman1111@gmail.com>
2022-08-02 18:40:28 +0700

    test PPO algo

--- train_vn_stock.py
+++ train_vn_stock.py
@@ -1,4 +1,5 @@
 from stable_baselines3.dqn import DQN
+from stable_baselines3.ppo import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
 import stock_env
 from stock_env.envs.vn_stock_env import VietnamStockEnv
@@ -13,30 +14,40 @@ env = VietnamStockEnv(
     max_lot=10,
     init_cash=5e3)
 
-model = DQN(
+# model = DQN(
+#     'MlpPolicy',
+#     env=env, 
+#     learning_rate=1e-3,
+#     gamma=0.999,
+#     buffer_size=100000,
+#     batch_size=128,
+#     train_freq=(4, "step"),
+#     gradient_steps=1,
+#     exploration_initial_eps=1,
+#     exploration_final_eps=0.1,
+#     learning_starts=5000,
+#     target_update_interval=1000,
+#     tensorboard_log='log',
+#     verbose=1,
+# )
+model = PPO(
     'MlpPolicy',
     env=env, 
     learning_rate=1e-3,
+    n_steps=64,
     gamma=0.999,
-    buffer_size=100000,
-    batch_size=128,
-    train_freq=(4, "step"),
-    gradient_steps=1,
-    exploration_initial_eps=1,
-    exploration_final_eps=0.1,
-    learning_starts=5000,
-    target_update_interval=1000,
+    batch_size=32,
     tensorboard_log='log',
-    verbose=1,
+    verbose=0,
 )
 model.learn(
-    total_timesteps=500000,
+    total_timesteps=1000000,
     eval_env=None,
     eval_freq=0,
     n_eval_episodes=0,
 )
 
-model.save('log/dqn_VietnamStock')
+model.save('log/ppo_VietnamStock')
 
 mean, std = evaluate_policy(model, env, n_eval_episodes=1)
 print(f"Mean reward: {mean:.2f} +/- {std: .2f}")
