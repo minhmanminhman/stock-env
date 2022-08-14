522fa12a5a5da6b80c372df896110e3b6f12d243
minhmanminhman <buingocminhman1111@gmail.com>
2022-08-14 20:14:50 +0700

    add VietnamStockV2Env

--- stock_env/envs/vn_stock_env.py
+++ stock_env/envs/vn_stock_env.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from xml.sax.handler import property_dom_node
 import numpy as np
 import pandas as pd
 import pandas_ta as ta
@@ -7,83 +8,41 @@ from .base_env import BaseStockEnv
 from ..utils import check_col
 from gym import spaces
 from empyrical import max_drawdown
+from ..strategy.trend_strategy import *
+import stable_baselines3
+# quantity = 1000, buy = 100 -> env chinh lai cai action la buy = 0 -> penalty mua du
+# them cac action truoc do 2 weeks
 
-# Create your own Custom Strategy
-TrendStrategy = ta.Strategy(
-    name="Trend Signal Strategy",
-    description="SMA 50,200, BBANDS, RSI, MACD and Volume SMA 20",
-    ta=[
-        {"kind": "percent_return", "length": 1},
-        {"kind": "sma", "length": 50},
-        {"kind": "ema", "length": 10},
-        {"kind": "ema", "length": 20},
-        {"kind": "donchian", "lower_length": 10, "upper_length": 10},
-        {"kind": "donchian", "lower_length": 20, "upper_length": 20},
-        {"kind": "donchian", "lower_length": 50, "upper_length": 50},
-        {"kind": "sma", "close": "volume", "length": 20, "prefix": "VOLUME"},
-    ]
-)
-
-# v2
-def trends(df: pd.DataFrame):
-    # create neccesary indicators
-    df.ta.strategy(TrendStrategy)
+class FeaturesExtractor:
     
-    # condition
-    trend_cond = (
-        (df['close'] > df['SMA_50']).astype(int)
-        + (df['EMA_10'] > df['EMA_20']).astype(int)
-        + (df['DCL_10_10'] > df['DCL_50_50']).astype(int)
-    ) >= 3
+    def __init__(self):
+        self.strategy = ta.Strategy(
+            name="Standard Technical Indicator in various research of automatic stock trading",
+            description="SMA MACD RSI CCI ADX Bollinger",
+            ta=[
+                {"kind": "sma", "length": 10},
+                {"kind": "rsi", "length": 14},
+                {"kind": "cci", "length": 14},
+                {"kind": "adx", "length": 14},
+                {"kind": "bbands"},
+                {"kind": "macd"},
+            ]
+        )
+        self.required_cols = set('time open high low close volume'.split())
+        self.feature_cols = "close SMA_10 RSI_14 CCI_14_0.015 BBL_5_2.0 BBU_5_2.0 MACD_12_26_9".split()
+        # indicators + cash + holding
+        self.feature_dim = len(self.feature_cols) + 2
     
-    return trend_cond
-
-def trends_confirm_entries(df: pd.DataFrame):
-    volume_breakout = (df['volume'] >= 1.25 * df['VOLUME_SMA_20']).astype(int)
-    
-    # candle pattern
-    entry_pattern = df.ta.cdl_pattern(
-        name=["closingmarubozu", "marubozu", "engulfing", "longline"], 
-        scalar=1).sum(axis=1).astype(int)
-    
-    exit_pattern = df.ta.cdl_pattern(
-        name=["closingmarubozu", "marubozu", "engulfing", "longline"], 
-        scalar=1).sum(axis=1).astype(int)
-    
-    df['entry'] = ((entry_pattern > 0) * volume_breakout * (df['close'] > df['DCU_10_10'].shift())).astype(bool).astype(int)
-    df['exit'] = (
-        (exit_pattern < 0) 
-        * volume_breakout 
-        # * (df['close'] < df['DCL_10_10'].shift())
-    ).astype(bool).astype(int)
-    
-    def modify_entry_exit(df):
-        df['trends_'] = np.nan
+    def preprocess(self, df):
+        check_col(df, self.required_cols)
+        df.sort_values(by='time', inplace=True)
+        # create indicators
+        df.ta.strategy(self.strategy)
         
-        try:
-            entry_idx = df[df['entry'] == 1].index[0]
-            df['trends_'].loc[entry_idx] = 1
-            
-            try:
-                exit_idx = df[df['exit'] == 1].index[-1]
-                if entry_idx <= exit_idx:
-                    df['trends_'].loc[exit_idx] = 1
-            except:
-                pass
-            finally:
-                df['trends_'] = df['trends_'].ffill()
-        except IndexError:
-            df['trends_'] = df['trends_'].fillna(0)
-        
-        try:
-            df['trends_'] = df['trends_'].interpolate(limit_area='inside', method='nearest')
-        except ValueError:
-            df['trends_'] = df['trends_'].fillna(0)
-        return df['trends_'].fillna(0) * df['trends']
-
-    df['original_trends'] = df['trends']
-    df['trends'] = df.groupby((df.trends != df.trends.shift()).cumsum()).apply(modify_entry_exit).to_list()
-    return df
+        df.dropna(inplace=True)
+        df = df.reset_index(drop=True)
+        df.sort_values(by='time', inplace=True)
+        return df[self.feature_cols]
 
 @dataclass
 class Position:
@@ -155,6 +114,12 @@ class VietnamStockEnv(BaseStockEnv):
     def nav(self):
         price = self.close.iloc[self._current_tick].item()
         return self.position.quantity * price
+
+    @property
+    def pnl_from_holding(self):
+        price = self.close.iloc[self._current_tick].item()
+        start_price = self.close.iloc[self._start_tick].item()
+        return price / start_price
     
     def reset(self) -> Tuple[np.ndarray, float, bool, dict]:
         obs = super().reset()
@@ -264,6 +229,11 @@ class VietnamStockEnv(BaseStockEnv):
         money_penalty = -self.cash * (1 - 0.04/365)
         return delta_vt - 0.5 * self.kappa * (delta_vt ** 2) + money_penalty
     
+    # def _calculate_reward(self, delta_vt: float) -> float:
+    #     money_penalty = -self.cash * (1 - 0.04/365)
+    #     baseline = self.init_cash * self.pnl_from_holding
+    #     return ((self.portfolio_value - baseline) + money_penalty)
+    
     def get_history(self):
         history_df = pd.DataFrame(self.history)
         data = self.df.merge(history_df, how='inner', on='time')
@@ -273,4 +243,197 @@ class VietnamStockEnv(BaseStockEnv):
         done =  super()._is_done()
         returns = pd.Series(self.history['portfolio_value']).pct_change()
         maximum_drawdown = max_drawdown(returns)
-        return done or (maximum_drawdown > 0.2)
+        return done or (maximum_drawdown < -0.5)
+
+class VietnamStockV2Env(BaseStockEnv):
+
+    def __init__(
+        self,
+        df: pd.DataFrame,
+        tick_size: float = 0.05,
+        lot_size: int = 100,
+        max_trade_lot: int = 5,
+        max_lot: int = 10,
+        kappa: float = 1e-4,
+        init_cash: float = 2e4,
+        random_seed: int = None,
+    ):
+        super().__init__(
+            df=df,
+            lot_size=lot_size,
+            max_trade_lot=max_trade_lot,
+            max_lot=max_lot,
+            init_cash=init_cash,
+            random_seed=random_seed
+        )
+        self.tick_size = tick_size
+        self.kappa = kappa
+        self.feature_extractor = FeaturesExtractor()
+        
+        self.features, self.df = self._preprocess(self.df)
+        self.close = self.df.close
+        self._end_tick = self.df.shape[0] - 1
+        self.observation_space = spaces.Box(
+            low=-np.inf, 
+            high=np.inf, 
+            shape=(self.feature_extractor.feature_dim,),
+            dtype=np.float64)
+        
+        self.action_space = spaces.Box(
+            low=0, 
+            high=1, 
+            shape=(1,),
+            dtype=np.float32)
+        
+        # Vietnam market condition
+        # can't short stock
+        self.min_quantity = 0
+        self.position = Position()
+    
+    @property
+    def nav(self):
+        price = self.close.iloc[self._current_tick].item()
+        return self.position.quantity * price
+
+    @property
+    def pnl_from_holding(self):
+        price = self.close.iloc[self._current_tick].item()
+        start_price = self.close.iloc[self._start_tick].item()
+        return price / start_price
+    
+    @property
+    def prev_price(self):
+        return self.close.iloc[self._current_tick-1].item()
+    
+    @property
+    def price(self):
+        return self.close.iloc[self._current_tick].item()
+    
+    def reset(self) -> Tuple[np.ndarray, float, bool, dict]:
+        obs = super().reset()
+        self.position.reset()
+        self.history.update({
+            'actions': [],
+            'delta_shares': [],
+            'delta_vt': [],
+            'total_reward': [],
+            'total_profit': [],
+            'portfolio_value': [],
+            'nav': [],
+            'cash': [],
+            'time': [],
+            'step_reward': []
+        })
+        return obs
+
+    def step(self, action: int) -> Tuple[np.ndarray, float, bool, dict]:
+        self._current_tick += 1
+        self.position.update_position()
+        delta_shares = self._decode_action(action)
+        
+        # enviroment constraint
+        modified_delta_shares = self._modify_quantity(delta_shares)
+        self.position.transact_trade(modified_delta_shares)
+        assert self.position.quantity <= self.max_quantity, f"delta_shares: {modified_delta_shares}, position: {self.position.__dict__}"
+        
+        # calculate reward
+        delta_vt = self._delta_vt(modified_delta_shares)
+        step_reward = self._calculate_reward(delta_vt)
+        self.total_reward += step_reward
+        
+        self._update_portfolio(modified_delta_shares)
+        
+        # always update history last
+        info = dict(
+            actions = action,
+            delta_shares = modified_delta_shares,
+            quantity = self.position.quantity,
+            delta_vt = delta_vt,
+            total_reward = self.total_reward,
+            total_profit = self.total_profit,
+            portfolio_value = self.portfolio_value,
+            nav = self.nav,
+            cash = self.cash,
+            time = self.df.time.iloc[self._current_tick],
+            step_reward = step_reward,
+        )
+        self._update_history(info)
+        return self._get_observation(), step_reward, self._is_done(), info
+    
+    def _preprocess(self, df):
+        required_col = set('time open high low close volume'.split())
+        check_col(df, required_col)
+        df.sort_values(by='time', inplace=True)
+        df = df.reset_index(drop=True)
+        features = self.feature_extractor.preprocess(df)
+        return features, df
+
+    def _modify_quantity(self, delta_shares: int) -> int:
+        """
+        modify quantity according to market contraint like T+3 or max quantity
+        """
+        _delta_shares = delta_shares
+        _quantity = self.position.quantity
+        if _delta_shares >= 0: # long or hold
+            if _quantity + _delta_shares > self.max_quantity:
+                delta_shares = max(self.max_quantity - _quantity, 0)
+            
+            _buy_value = self.prev_price * delta_shares + self._total_cost(delta_shares)
+            
+            while _buy_value > self.cash:
+                delta_shares = (int(delta_shares / self.lot_size) - 1) * self.lot_size
+                _buy_value = self.prev_price * delta_shares + self._total_cost(delta_shares)
+                
+        else: # short
+            short_quantity = min(self.position.on_hand, abs(_delta_shares))
+            delta_shares = -short_quantity
+        return delta_shares
+
+    def _get_observation(self) -> np.ndarray:
+        features = self.features.iloc[self._current_tick].values
+        quantity = self.position.quantity
+        obs = np.append(features, quantity).astype(np.float32)
+        obs = np.append(obs, self.cash).astype(np.float32)
+        return obs
+    
+    def _spread_cost(self, delta_shares: int) -> float:
+        return abs(delta_shares) * self.tick_size
+
+    def _impact_cost(self, delta_shares: int) -> float:
+        return delta_shares ** 2 * self.tick_size / self.lot_size
+    
+    def _total_cost(self, delta_shares: int) -> float:
+        return self._spread_cost(delta_shares) + self._impact_cost(delta_shares)
+    
+    def _delta_vt(self, delta_shares: int) -> float:
+        return self.position.quantity * (self.price - self.prev_price) - self._total_cost(delta_shares)
+    
+    # def _decode_action(self, action: int) -> int:
+    #     return np.take(self.quantity_choice, action)
+    
+    def _decode_action(self, action: float) -> int:
+        stock_percent = self.nav / self.portfolio_value
+        diff_percent = action - stock_percent
+        diff_value = diff_percent * self.portfolio_value
+        diff_shares = int((diff_value / self.prev_price) / self.lot_size) * self.lot_size
+        return np.clip(diff_shares, self.min_quantity, self.max_quantity)
+
+    def _calculate_reward(self, delta_vt: float) -> float:
+        money_penalty = -self.cash * (1 - 0.04/365)
+        return delta_vt - 0.5 * self.kappa * (delta_vt ** 2) + money_penalty
+    
+    # def _calculate_reward(self, delta_vt: float) -> float:
+    #     money_penalty = -self.cash * (1 - 0.04/365)
+    #     baseline = self.init_cash * self.pnl_from_holding
+    #     return ((self.portfolio_value - baseline) + money_penalty)
+    
+    def get_history(self):
+        history_df = pd.DataFrame(self.history)
+        data = self.df.merge(history_df, how='inner', on='time')
+        return data
+    
+    def _is_done(self):
+        done =  super()._is_done()
+        returns = pd.Series(self.history['portfolio_value']).pct_change()
+        maximum_drawdown = max_drawdown(returns)
+        return done or (maximum_drawdown < -0.5)
