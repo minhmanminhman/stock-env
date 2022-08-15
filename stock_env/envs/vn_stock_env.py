9f97c4a93b0e4119b0dd29c76037ab00230278eb
minhmanminhman <buingocminhman1111@gmail.com>
2022-08-15 18:39:26 +0700

    continuous action

--- stock_env/envs/vn_stock_env.py
+++ stock_env/envs/vn_stock_env.py
@@ -31,7 +31,7 @@ class FeaturesExtractor:
         self.required_cols = set('time open high low close volume'.split())
         self.feature_cols = "close SMA_10 RSI_14 CCI_14_0.015 BBL_5_2.0 BBU_5_2.0 MACD_12_26_9".split()
         # indicators + cash + holding
-        self.feature_dim = len(self.feature_cols) + 2
+        self.feature_dim = len(self.feature_cols)
     
     def preprocess(self, df):
         check_col(df, self.required_cols)
@@ -50,11 +50,16 @@ class Position:
     t1_quantity: int = 0
     t2_quantity: int = 0
     on_hand: int = 0
+    avg_price: float = 0
     
     @property
     def quantity(self):
         quantity = self.t0_quantity + self.t1_quantity + self.t2_quantity + self.on_hand
         return quantity
+    
+    @property
+    def invested_value(self):
+        return self.avg_price * self.quantity
     
     def update_position(self):
         self.on_hand += self.t2_quantity
@@ -62,9 +67,19 @@ class Position:
         self.t1_quantity = self.t0_quantity
         self.t0_quantity = 0
     
-    def transact_trade(self, delta_shares):
+    def transact_trade(self, trade: dict):
+        delta_shares = trade.get('delta_shares')
         if delta_shares >= 0: # long or hold
+            avg_price = trade.get('avg_price')
+            buy_value = avg_price * delta_shares
+            prev_invested_value = self.invested_value
+            
+            # update new quantity
             self.t0_quantity = delta_shares
+            if self.quantity > 0:
+                self.avg_price = (buy_value + prev_invested_value) / self.quantity
+            elif self.quantity == 0:
+                self.avg_price = 0
         else: # short
             self.on_hand = self.on_hand + delta_shares
             assert self.on_hand >= 0
@@ -269,18 +284,17 @@ class VietnamStockV2Env(BaseStockEnv):
         self.tick_size = tick_size
         self.kappa = kappa
         self.feature_extractor = FeaturesExtractor()
-        
         self.features, self.df = self._preprocess(self.df)
         self.close = self.df.close
         self._end_tick = self.df.shape[0] - 1
         self.observation_space = spaces.Box(
             low=-np.inf, 
             high=np.inf, 
-            shape=(self.feature_extractor.feature_dim,),
+            shape=(self.feature_extractor.feature_dim + 2,),
             dtype=np.float64)
         
         self.action_space = spaces.Box(
-            low=0, 
+            low=-1, 
             high=1, 
             shape=(1,),
             dtype=np.float32)
@@ -302,6 +316,10 @@ class VietnamStockV2Env(BaseStockEnv):
         return price / start_price
     
     @property
+    def invested_value(self):
+        return self.cash + self.position.invested_value
+    
+    @property
     def prev_price(self):
         return self.close.iloc[self._current_tick-1].item()
     
@@ -311,6 +329,7 @@ class VietnamStockV2Env(BaseStockEnv):
     
     def reset(self) -> Tuple[np.ndarray, float, bool, dict]:
         obs = super().reset()
+        self._current_tick = np.random.randint(self._start_tick, self._end_tick)
         self.position.reset()
         self.history.update({
             'actions': [],
@@ -322,18 +341,25 @@ class VietnamStockV2Env(BaseStockEnv):
             'nav': [],
             'cash': [],
             'time': [],
-            'step_reward': []
+            'step_reward': [],
+            'invested_value': [],
+            'avg_price': [],
         })
         return obs
 
     def step(self, action: int) -> Tuple[np.ndarray, float, bool, dict]:
         self._current_tick += 1
         self.position.update_position()
+        action = self.unscale_action(action)
         delta_shares = self._decode_action(action)
         
         # enviroment constraint
         modified_delta_shares = self._modify_quantity(delta_shares)
-        self.position.transact_trade(modified_delta_shares)
+        trade = {
+            'delta_shares': modified_delta_shares,
+            'avg_price': self.prev_price
+        }
+        self.position.transact_trade(trade)
         assert self.position.quantity <= self.max_quantity, f"delta_shares: {modified_delta_shares}, position: {self.position.__dict__}"
         
         # calculate reward
@@ -345,7 +371,7 @@ class VietnamStockV2Env(BaseStockEnv):
         
         # always update history last
         info = dict(
-            actions = action,
+            actions = action.item(),
             delta_shares = modified_delta_shares,
             quantity = self.position.quantity,
             delta_vt = delta_vt,
@@ -354,8 +380,10 @@ class VietnamStockV2Env(BaseStockEnv):
             portfolio_value = self.portfolio_value,
             nav = self.nav,
             cash = self.cash,
-            time = self.df.time.iloc[self._current_tick],
+            time = self.df['time'].iloc[self._current_tick],
             step_reward = step_reward,
+            invested_value = self.invested_value,
+            avg_price = self.position.avg_price,
         )
         self._update_history(info)
         return self._get_observation(), step_reward, self._is_done(), info
@@ -366,6 +394,7 @@ class VietnamStockV2Env(BaseStockEnv):
         df.sort_values(by='time', inplace=True)
         df = df.reset_index(drop=True)
         features = self.feature_extractor.preprocess(df)
+        features = features.reset_index(drop=True)
         return features, df
 
     def _modify_quantity(self, delta_shares: int) -> int:
@@ -391,9 +420,10 @@ class VietnamStockV2Env(BaseStockEnv):
 
     def _get_observation(self) -> np.ndarray:
         features = self.features.iloc[self._current_tick].values
-        quantity = self.position.quantity
-        obs = np.append(features, quantity).astype(np.float32)
-        obs = np.append(obs, self.cash).astype(np.float32)
+        cash_percent = self.cash / self.portfolio_value
+        stock_percent = self.nav / self.portfolio_value
+        obs = np.append(features, cash_percent).astype(np.float32)
+        obs = np.append(obs, stock_percent).astype(np.float32)
         return obs
     
     def _spread_cost(self, delta_shares: int) -> float:
@@ -408,27 +438,21 @@ class VietnamStockV2Env(BaseStockEnv):
     def _delta_vt(self, delta_shares: int) -> float:
         return self.position.quantity * (self.price - self.prev_price) - self._total_cost(delta_shares)
     
-    # def _decode_action(self, action: int) -> int:
-    #     return np.take(self.quantity_choice, action)
-    
     def _decode_action(self, action: float) -> int:
-        stock_percent = self.nav / self.portfolio_value
-        diff_percent = action - stock_percent
-        diff_value = diff_percent * self.portfolio_value
+        target_cash = action * self.portfolio_value
+        diff_value = self.cash - target_cash
+        # if diff_value > 0 -> buy more shares
+        # if diff_value < 0 -> sell shares
         diff_shares = int((diff_value / self.prev_price) / self.lot_size) * self.lot_size
-        return np.clip(diff_shares, self.min_quantity, self.max_quantity)
+        return diff_shares
 
     def _calculate_reward(self, delta_vt: float) -> float:
-        money_penalty = -self.cash * (1 - 0.04/365)
-        return delta_vt - 0.5 * self.kappa * (delta_vt ** 2) + money_penalty
-    
-    # def _calculate_reward(self, delta_vt: float) -> float:
-    #     money_penalty = -self.cash * (1 - 0.04/365)
-    #     baseline = self.init_cash * self.pnl_from_holding
-    #     return ((self.portfolio_value - baseline) + money_penalty)
+        # money_penalty = -self.cash * (1 - 0.04/365)
+        return delta_vt #- 0.5 * self.kappa * (delta_vt ** 2) + money_penalty
     
     def get_history(self):
         history_df = pd.DataFrame(self.history)
+        history_df = history_df.astype({'time':'datetime64[ns]'})
         data = self.df.merge(history_df, how='inner', on='time')
         return data
     
@@ -437,3 +461,18 @@ class VietnamStockV2Env(BaseStockEnv):
         returns = pd.Series(self.history['portfolio_value']).pct_change()
         maximum_drawdown = max_drawdown(returns)
         return done or (maximum_drawdown < -0.5)
+    
+    def _update_portfolio(self, delta_shares: int) -> float:
+        # buy/sell at close price
+        self.cash -= (delta_shares * self.prev_price + self._total_cost(delta_shares))
+        assert self.cash >= 0
+        
+    def unscale_action(self, scaled_action: np.ndarray) -> np.ndarray:
+        """
+        Rescale the action from [-1, 1] to [low, high]
+        (no need for symmetric action space)
+
+        :param scaled_action: Action to un-scale
+        """
+        low, high = 0, 1
+        return low + (0.5 * (scaled_action + 1.0) * (high - low))
