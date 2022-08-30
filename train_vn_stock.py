5d5457ccd698e3a6e152fd038556c10e98973c6e
minhmanminhman <buingocminhman1111@gmail.com>
2022-08-30 14:18:26 +0700

    add buffer wrapper

--- train_vn_stock.py
+++ train_vn_stock.py
@@ -7,13 +7,15 @@ from stock_env.envs.vn_stock_env import *
 from stock_env.feature.feature_extractor import *
 import mt4_hst
 from stable_baselines3.common.env_checker import check_env
+from stock_env.wrappers.stack_obs import BufferWrapper
 
 if __name__ == '__main__':
     env = 'VietnamStockContinuousEnv'
     algo = 'ppo'
-    ticker = "SSI"
+    ticker = "VNM"
     path = "../stock_datasets/"
     feature_extractor = TrendFeatures()
+    n_steps = 10
     name = f"{algo}_{env}_{feature_extractor.__class__.__name__}_{ticker}"
     
     env = VietnamStockContinuousEnv(
@@ -22,6 +24,7 @@ if __name__ == '__main__':
         init_cash = 100e3,
         ticker = ticker,
     )
+    # env = BufferWrapper(env, n_steps)
     check_env(env)
     # model = DQN(
     #     'MlpPolicy',
@@ -42,7 +45,7 @@ if __name__ == '__main__':
     model = PPO(
         'MlpPolicy',
         env=env, 
-        learning_rate=1e-4,
+        learning_rate=1e-3,
         ent_coef=0.01,
         n_steps=20,
         gamma=0.99,
@@ -64,6 +67,7 @@ if __name__ == '__main__':
     #     tensorboard_log='log',
     #     verbose=1,
     # )
+    print(model.policy)
     model.learn(
         total_timesteps=100000,
         eval_env=None,
@@ -78,8 +82,7 @@ if __name__ == '__main__':
 
     # run model to get detailed information in the enviroment
     done = False
-    obs = env.reset()
-    env._current_tick = 0
+    obs = env.reset(current_tick=0)
     while not done:
         action, _ = model.predict(obs, deterministic=True)
         obs, _, done, _ = env.step(action)
