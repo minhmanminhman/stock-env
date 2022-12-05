7f3ff938ec5911d3f20dfacf2456a1278b9243c6
manbnm <buingocminhman1111@gmail.com>
2022-12-05 22:34:46 +0700

    change stack obs to gymnasium style

--- stock_env/wrappers/stack_obs.py
+++ stock_env/wrappers/stack_obs.py
@@ -7,16 +7,15 @@ class StackObs(gym.ObservationWrapper):
         self.dtype = dtype
         self.n_steps = n_steps
         old_space = env.observation_space
+        # self.observation_space = gym.spaces.Box(
+        #     old_space.low.repeat(n_steps).reshape(n_steps, -1),
+        #     old_space.high.repeat(n_steps).reshape(n_steps, -1))
         self.observation_space = gym.spaces.Box(
-            old_space.low.repeat(n_steps).reshape(n_steps, -1),
-            old_space.high.repeat(n_steps).reshape(n_steps, -1))
+            old_space.low.repeat(n_steps), old_space.high.repeat(n_steps))
         self.buffer = np.zeros_like(
-            self.observation_space.low, dtype=self.dtype)
-
-    def reset(self, **kwargs):
-        return self.observation(self.env.reset(**kwargs))
+            self.observation_space.low.reshape(n_steps, -1), dtype=self.dtype)
 
     def observation(self, observation):
         self.buffer[:-1] = self.buffer[1:]
         self.buffer[-1] = observation
-        return self.buffer
+        return self.buffer.flatten()
