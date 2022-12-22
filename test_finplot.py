9cd74369d7b44cd8dff130d6fa38df280d64ca3c
manbnm <buingocminhman1111@gmail.com>
2022-12-22 15:22:45 +0700

    add test finplot

---
+++ test_finplot.py
@@ -0,0 +1,14 @@
+import finplot as fplt
+import pandas as pd
+from stock_env.data_loader.vndirect_loader import VNDDataLoader
+
+vndloader = VNDDataLoader(
+    symbols=['HCM'],
+    start='2016-01-01',
+    end='2022-12-31'
+)
+data = vndloader.download()
+
+data = data.set_index('date')
+fplt.candlestick_ochl(data[['adOpen', 'adClose', 'adHigh', 'adLow']])
+fplt.show()
