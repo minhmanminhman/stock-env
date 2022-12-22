b2618f44eed77f3566d5aa52d35545107612e0de
manbnm <buingocminhman1111@gmail.com>
2022-12-22 15:17:39 +0700

    asign cash variable

--- stock_env/envs/base_env.py
+++ stock_env/envs/base_env.py
@@ -7,37 +7,39 @@ from dataclasses import dataclass
 
 from ..data_loader import BaseDataLoader
 
+
 @dataclass
 class Position:
     t0_quantity: int = 0
     t1_quantity: int = 0
     t2_quantity: int = 0
     on_hand: int = 0
-    
+
     @property
     def quantity(self):
         quantity = self.t0_quantity + self.t1_quantity + self.t2_quantity + self.on_hand
         return quantity
-    
+
     def update_position(self):
         self.on_hand += self.t2_quantity
         self.t2_quantity = self.t1_quantity
         self.t1_quantity = self.t0_quantity
         self.t0_quantity = 0
-    
+
     def transact_trade(self, delta_shares):
-        if delta_shares >= 0: # long or hold
+        if delta_shares >= 0:  # long or hold
             self.t0_quantity = delta_shares
-        else: # short
+        else:  # short
             self.on_hand = self.on_hand + delta_shares
             assert self.on_hand >= 0
-    
+
     def reset(self):
         self.t0_quantity = self.t1_quantity = self.t2_quantity = self.on_hand = 0
 
+
 class BaseVietnamStockEnv(gym.Env):
 
-    metadata = {'render.modes': ['human']}
+    metadata = {"render.modes": ["human"]}
 
     def __init__(
         self,
@@ -47,7 +49,7 @@ class BaseVietnamStockEnv(gym.Env):
     ):
         self.seed(random_seed)
         self.data_loader = data_loader
-        self.init_cash = init_cash
+        self.cash = self.init_cash = init_cash
         self.history = None
 
     def seed(self, seed=None):
@@ -60,11 +62,11 @@ class BaseVietnamStockEnv(gym.Env):
 
         for key, value in info.items():
             self.history[key].append(value)
-    
+
     @abstractmethod
     def reset(self) -> Tuple[np.ndarray, float, bool, dict]:
         raise NotImplementedError
-    
+
     @abstractmethod
     def step(self, action) -> Tuple[np.ndarray, float, bool, dict]:
         raise NotImplementedError
