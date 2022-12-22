cefdc6593225429703616cdb5997601cf3597179
manbnm <buingocminhman1111@gmail.com>
2022-12-22 20:57:26 +0700

    add stack_obs wrapper

--- stock_env/envs/__init__.py
+++ stock_env/envs/__init__.py
@@ -2,59 +2,70 @@ from .base_env import *
 from .random_stock import *
 from stock_env.envs.task_stock import *
 from stock_env.envs.vec_task_env import *
+from stock_env.wrappers import StackAndSkipObs
 from stock_env.common.common_utils import open_config
 from gymnasium import register
 import pathlib
-config_path = pathlib.Path(__file__).parent.parent.parent.joinpath('configs').resolve()
+
+config_path = pathlib.Path(__file__).parent.parent.parent.joinpath("configs").resolve()
+
 
 def create_env(name):
     def make_env():
         from ..data_loader import RandomStockLoader
         from ..wrappers import StackObs
         import pathlib
-        path = pathlib.Path(__file__).parent.parent.joinpath('datasets').resolve()
+
+        path = pathlib.Path(__file__).parent.parent.joinpath("datasets").resolve()
         data_loader = RandomStockLoader.load(f"{path}/{name}")
         env = RandomStockEnv(data_loader)
         env = StackObs(env, n_steps=5)
         env = gym.wrappers.RecordEpisodeStatistics(env)
         return env
+
     return make_env
 
-def make_task_env(name, seed=None, gamma=0.99, env_kwargs={}):
+
+def make_task_env(name, gamma=0.99, env_kwargs={}):
     def _thunk():
         from stock_env.data_loader import USTaskLoader
         import pathlib
-        path = pathlib.Path(__file__).parent.parent.joinpath('datasets').resolve()
-        
+
+        path = pathlib.Path(__file__).parent.parent.joinpath("datasets").resolve()
+
         task_loader = USTaskLoader.load(f"{path}/{name}")
         env = TaskStockEnv(task_loader, **env_kwargs)
-        
+
+        # temp reset_task to create env, should be reset later
         _task = env.sample_task()
         env.reset_task(_task)
-        
+
         # wrap env
+        # env = StackAndSkipObs(env, num_stack=5, num_skip=3)
         env = gym.wrappers.RecordEpisodeStatistics(env)
         env = gym.wrappers.ClipAction(env)
         env = gym.wrappers.NormalizeObservation(env)
         env = gym.wrappers.TransformObservation(env, lambda obs: np.clip(obs, -10, 10))
         env = gym.wrappers.NormalizeReward(env, gamma=gamma)
         env = gym.wrappers.TransformReward(env, lambda reward: np.clip(reward, -10, 10))
-        env.seed(seed)
         return env
+
     return _thunk
+
 
 def make_env(name, seed=None, gamma=0.99):
     def _thunk():
         from stock_env.data_loader import USTaskLoader
         import pathlib
-        path = pathlib.Path(__file__).parent.parent.joinpath('datasets').resolve()
-        
+
+        path = pathlib.Path(__file__).parent.parent.joinpath("datasets").resolve()
+
         task_loader = USTaskLoader.load(f"{path}/{name}")
         env = TaskStockEnv(task_loader)
-        
-        _task = env.sample_task()
+
+        _task = env.sample_task(seed=seed)
         env.reset_task(_task)
-        
+
         # wrap env
         env = gym.wrappers.RecordEpisodeStatistics(env)
         env = gym.wrappers.ClipAction(env)
@@ -64,51 +75,61 @@ def make_env(name, seed=None, gamma=0.99):
         env = gym.wrappers.TransformReward(env, lambda reward: np.clip(reward, -10, 10))
         env.seed(seed)
         return env
+
     return _thunk
+
 
 register(
     id=f"FinService-v0",
-    entry_point=create_env('finservice_data_loader'),
+    entry_point=create_env("finservice_data_loader"),
 )
 
 register(
     id=f"RandomVN30-v0",
-    entry_point=create_env('30stocks'),
+    entry_point=create_env("30stocks"),
 )
 
 register(
     id=f"SP500-v0",
     entry_point=make_task_env(
-        name='sp500',
-        env_kwargs=open_config(f'{config_path}/envs.yaml', env_id="SP500-v0", is_args=False)
+        name="sp500",
+        env_kwargs=open_config(
+            f"{config_path}/envs.yaml", env_id="SP500-v0", is_args=False
+        ),
     ),
 )
 
 register(
     id=f"VNALL-v0",
     entry_point=make_task_env(
-        name='vnall',
-        env_kwargs=open_config(f'{config_path}/envs.yaml', env_id="VNALL-v0", is_args=False)
+        name="vnall",
+        env_kwargs=open_config(
+            f"{config_path}/envs.yaml", env_id="VNALL-v0", is_args=False
+        ),
     ),
 )
 
 register(
     id=f"FAANGTask-v0",
-    entry_point=make_task_env('faang_task_loader'),
+    entry_point=make_task_env("faang_task_loader"),
 )
 
 register(
     id=f"MiniFAANG-v0",
     entry_point=make_task_env(
-        name='mini_faang', 
-        env_kwargs=open_config(f'{config_path}/envs.yaml', env_id="MiniFAANG-v0", is_args=False)
+        name="mini_faang",
+        env_kwargs=open_config(
+            f"{config_path}/envs.yaml", env_id="MiniFAANG-v0", is_args=False
+        ),
     ),
 )
 
 register(
     id="MiniVNStock-v0",
     entry_point=make_task_env(
-        name='mini_vnstock', 
-        env_kwargs=open_config(f'{config_path}/envs.yaml', env_id="MiniVNStock-v0", is_args=False)
+        name="mini_vnstock",
+        env_kwargs=open_config(
+            f"{config_path}/envs.yaml", env_id="MiniVNStock-v0", is_args=False
+        ),
     ),
 )
--- stock_env/wrappers/stack_obs.py
+++ stock_env/wrappers/stack_obs.py
@@ -1,21 +1,36 @@
 import gymnasium as gym
 import numpy as np
+from collections import deque
+from gymnasium import spaces
 
-class StackObs(gym.ObservationWrapper):
-    def __init__(self, env, n_steps, dtype=np.float32):
-        super(StackObs, self).__init__(env)
+
+class StackAndSkipObs(gym.Wrapper):
+    def __init__(self, env, num_stack, num_skip, dtype=np.float32):
+        super(StackAndSkipObs, self).__init__(env)
         self.dtype = dtype
-        self.n_steps = n_steps
+        self.num_stack = num_stack
+        self.num_skip = num_skip
         old_space = env.observation_space
-        # self.observation_space = gym.spaces.Box(
-        #     old_space.low.repeat(n_steps).reshape(n_steps, -1),
-        #     old_space.high.repeat(n_steps).reshape(n_steps, -1))
         self.observation_space = gym.spaces.Box(
-            old_space.low.repeat(n_steps), old_space.high.repeat(n_steps))
-        self.buffer = np.zeros_like(
-            self.observation_space.low.reshape(n_steps, -1), dtype=self.dtype)
+            old_space.low.repeat(num_stack), old_space.high.repeat(num_stack)
+        )
+        self.obs_buffer = deque(maxlen=num_stack)
 
-    def observation(self, observation):
-        self.buffer[:-1] = self.buffer[1:]
-        self.buffer[-1] = observation
-        return self.buffer.flatten()
+    def reset(self, **kwargs):
+        obs, info = self.env.reset(**kwargs)
+        [self.obs_buffer.append(obs) for _ in range(self.num_stack)]
+        return self._get_observation(), info
+
+    def step(self, action):
+        total_reward, terminated, truncated, info = 0.0, False, False, {}
+        for _ in range(self.num_skip):
+            observation, reward, terminated, truncated, info = self.env.step(action)
+            total_reward += reward
+            self.obs_buffer.append(observation)
+
+            if terminated or truncated:
+                break
+        return self._get_observation(), total_reward, terminated, truncated, info
+
+    def _get_observation(self):
+        return spaces.flatten(self.observation_space, self.obs_buffer)
