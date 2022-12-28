43700790e983aad9523d2d6b0d63b6b2f97c6df0
manbnm <buingocminhman1111@gmail.com>
2022-12-28 10:02:59 +0700

    add meta vector env for half cheetah

--- stock_env/envs/vec_task_env.py
+++ stock_env/envs/vec_task_env.py
@@ -3,7 +3,7 @@ from typing import List
 import numpy as np
 
 
-class MetaVectorEnv(SyncVectorEnv):
+class MetaVectorStockEnv(SyncVectorEnv):
 
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
@@ -24,3 +24,21 @@ class MetaVectorEnv(SyncVectorEnv):
     def train(self, mode=True):
         for env in self.envs:
             env.data_loader.train(mode)
+
+
+class MetaVectorEnv(SyncVectorEnv):
+
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
+
+    def __init__(self, env_fns, **kwargs):
+        super().__init__(env_fns, **kwargs)
+
+    def reset_task(self, task: str):
+        for env in self.envs:
+            env.reset_task(task)
+
+    def sample_task(self, num_tasks) -> List[str]:
+        return self.envs[0].sample_task(num_tasks)
+
+    def train(self, mode=True):
+        pass
